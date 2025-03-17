69b21d36fb27c5c60f886df57a1a9aae32cb5c50
megalaisathanathan <megalai.sathanathan@renoirgroup.com>
2025-03-17 14:14:55 +0800

    First push

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "8f09db99-9562-48d3-ae5a-6e7865d59b73")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "W1R8Q~jCdbUAvxxUaD2f8yzLgWaR-MlmUfibBb_8")
+    APP_ID = os.environ.get("MicrosoftAppId", "")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
