a31822a26e9912141885eb9fec250298aa6b2a12
megalaisathanathan <megalai.sathanathan@renoirgroup.com>
2025-04-28 15:14:56 +0800

    display of extract in telegram

---
+++ app.py
@@ -0,0 +1,79 @@
+import sys
+import traceback
+from datetime import datetime, timezone
+from aiohttp import web
+from aiohttp.web import Request, Response, json_response
+from botbuilder.core import BotFrameworkAdapter, BotFrameworkAdapterSettings, TurnContext
+from botbuilder.core.integration import aiohttp_error_middleware
+from botbuilder.schema import Activity, ActivityTypes
+from botbuilder.core import UserState, MemoryStorage
+from bot import MyBot
+from config import DefaultConfig
+
+# Application configuration
+CONFIG = DefaultConfig()
+
+SETTINGS = BotFrameworkAdapterSettings(CONFIG.APP_ID, CONFIG.APP_PASSWORD)
+ADAPTER = BotFrameworkAdapter(SETTINGS)
+
+# Error handler
+async def on_error(context: TurnContext, error: Exception):
+    print(f"\n [on_turn_error] unhandled error: {error}", file=sys.stderr)
+    traceback.print_exc()
+
+    await context.send_activity("The bot encountered an error or bug.")
+    await context.send_activity("To continue to run this bot, please fix the bot source code.")
+
+    if context.activity.channel_id == "emulator":
+        trace_activity = Activity(
+            label="TurnError",
+            name="on_turn_error Trace",
+            timestamp=datetime.now(timezone.utc),
+            type=ActivityTypes.trace,
+            value=f"{error}",
+            value_type="https://www.botframework.com/schemas/error",
+        )
+        await context.send_activity(trace_activity)
+
+ADAPTER.on_turn_error = on_error
+
+# Set up memory storage and user state
+MEMORY = MemoryStorage()
+USER_STATE = UserState(MEMORY)
+
+# Initialize bot
+BOT = MyBot(USER_STATE)
+
+# POST handler for messages
+async def messages(req: Request) -> Response:
+    if req.method == "POST":
+        if "application/json" in req.headers.get("Content-Type", ""):
+            body = await req.json()
+        else:
+            return Response(status=415)
+
+        activity = Activity().deserialize(body)
+        auth_header = req.headers.get("Authorization", "")
+
+        response = await ADAPTER.process_activity(activity, auth_header, BOT.on_turn)
+        if response:
+            return json_response(data=response.body, status=response.status)
+        return Response(status=201)
+    else:
+        return Response(status=405)  # Method Not Allowed
+
+    return Response(status=405)  # Method Not Allowed for anything other than POST
+                                        
+# Setup and run app
+def init_func(argv):
+    APP = web.Application(middlewares=[aiohttp_error_middleware])
+    APP.router.add_post("/api/messages", messages)
+    return APP  
+
+
+if __name__ == "__main__":
+    APP = init_func(None)
+    try:
+        web.run_app(APP, host="0.0.0.0", port=CONFIG.PORT)
+    except Exception as error:
+        raise error
